96881dde00a7bf69d4ca83c8d92e8168fb4a3cde
Kumudi Iroshini <abeygunawardena123@gmail.com>
2023-12-30 12:49:48 +0530

    Complete Form

--- front-end/to-do-app/src/form/Form.tsx
+++ front-end/to-do-app/src/form/Form.tsx
@@ -1,16 +1,27 @@
 import './Form.css'
 import React, {useRef, useState} from "react";
+import {saveTask} from "../service/TaskService.tsx";
+import {useUser} from "../context/UserContext.tsx";
+import {useTaskDispatcher} from "../context/TaskContext.tsx";
+import {TaskDTO} from "../dto/TaskDTO.ts";
 
 export function Form() {
 
+    const user = useUser();
     const [value, setValue] = useState("");
     const txtRef= useRef<HTMLInputElement>(null);
+    const taskDispatcher = useTaskDispatcher();
 
     function handleSubmit(e:React.FormEvent){
         e.preventDefault();
-
-        setValue("");
-        txtRef.current!.focus();
+        if(!value.trim()) return;
+        saveTask(new TaskDTO(null,value,null,user?.email!)).then(task => {
+            taskDispatcher({type:'add',task});
+            setValue("");
+            txtRef.current!.focus();
+        }).catch(err => {
+            alert("Failed to save the task, try again!");
+        });
     }
 
     return (
@@ -18,6 +29,7 @@ export function Form() {
               className="d-flex g-2 p-2 border-bottom">
 
             <input className="form-control"
+                   ref={txtRef}
                    value={value}
                    onChange={e=> setValue(e.target.value)}
                    type="text" placeholder="Eg: Task 1"/>
